7b5d8a7ac9193d09d2d55146046ada26b1a38979
Provissy <provissy@gmail.com>
2020-03-23 15:36:38 +0800

    now handles ERROR output, add shortcut to open dest folder

--- wpf-youtube-dl/MainWindow.xaml.cs
+++ wpf-youtube-dl/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Management.Automation;
@@ -26,16 +27,26 @@ namespace wpf_youtube_dl
     {
 
         private PSDataCollection<PSObject> outputCollection = new PSDataCollection<PSObject>();
+        
         private PowerShell ps;
         private IAsyncResult iasresult;
         public MainWindow()
         {
             InitializeComponent();
             tb_Path.Text = Directory.GetCurrentDirectory();
+
+            // For my own test only.
+
+            //tb_Proxy.Text = "127.0.0.1:30997";
+            //tb_Path.Text = "R:\\! Youtube Video\\";
         }
 
         private void btn_StartDownload_Click(object sender, RoutedEventArgs e)
         {
+            // Check path and try to fix first
+            if (tb_Path.Text.Substring(tb_Path.Text.Length - 1) != "\\")
+                tb_Path.Text += "\\";
+
             ps = null;
             iasresult = null;
             string cmdScript = getCommand("down");
@@ -55,7 +66,9 @@ namespace wpf_youtube_dl
             ps.AddScript(cmdScript);
             ps.Streams.Error.DataAdded += Error_DataAdded;
             outputCollection.DataAdded += outputCollection_DataAdded;
+            
             iasresult = ps.BeginInvoke<PSObject, PSObject>(null, outputCollection);
+            
         }
 
         private string getCommand(string type)
@@ -66,22 +79,22 @@ namespace wpf_youtube_dl
                 {
                     if (!String.IsNullOrEmpty(tb_Proxy.Text))
                     {
-                        return ".\\youtube-dl.exe --proxy " + tb_Proxy.Text + " -f bestvideo[vcodec^=av01]+bestaudio[acodec^=opus] -o \'" + tb_Path.Text + "%(title)s-%(id)s.%(vcodec)s.%(ext)s\' " + tb_Link.Text;
+                        return ".\\youtube-dl.exe --proxy " + tb_Proxy.Text + " -f bestvideo[vcodec^=av01]+bestaudio[acodec^=opus] -o \"" + tb_Path.Text + "%(title)s-%(id)s.%(vcodec)s.%(ext)s\" " + tb_Link.Text;
                     }
                     else
                     {
-                        return ".\\youtube-dl.exe -f bestvideo[vcodec^=av01]+bestaudio[acodec^=opus] -o \'" + tb_Path.Text + "%(title)s-%(id)s.%(vcodec)s.%(ext)s\' " + tb_Link.Text;
+                        return ".\\youtube-dl.exe -f bestvideo[vcodec^=av01]+bestaudio[acodec^=opus] -o \"" + tb_Path.Text + "%(title)s-%(id)s.%(vcodec)s.%(ext)s\" " + tb_Link.Text;
                     }
                 }
                 else
                 {
                     if (!String.IsNullOrEmpty(tb_Proxy.Text))
                     {
-                        return ".\\youtube-dl.exe --proxy " + tb_Proxy.Text + " -f bestvideo[vcodec^=vp9]+bestaudio[acodec^=opus] -o \'" + tb_Path.Text + "%(title)s-%(id)s.%(vcodec)s.%(ext)s\' " + tb_Link.Text;
+                        return ".\\youtube-dl.exe --proxy " + tb_Proxy.Text + " -f bestvideo[vcodec^=vp9]+bestaudio[acodec^=opus] -o \"" + tb_Path.Text + "%(title)s-%(id)s.%(vcodec)s.%(ext)s\" " + tb_Link.Text;
                     }
                     else
                     {
-                        return ".\\youtube-dl.exe -f bestvideo[vcodec^=vp9]+bestaudio[acodec^=opus] -o \'" + tb_Path.Text + "%(title)s-%(id)s.%(vcodec)s.%(ext)s\' " + tb_Link.Text;
+                        return ".\\youtube-dl.exe -f bestvideo[vcodec^=vp9]+bestaudio[acodec^=opus] -o \"" + tb_Path.Text + "%(title)s-%(id)s.%(vcodec)s.%(ext)s\" " + tb_Link.Text;
                     }
                 }
             }
@@ -101,7 +114,13 @@ namespace wpf_youtube_dl
 
         void Error_DataAdded(object sender, DataAddedEventArgs e)
         {
-
+            tb_Out.Dispatcher.Invoke(() =>
+            {
+                foreach(ErrorRecord erc in ps.Streams.Error.ReadAll())
+                {
+                    tb_Out.Text += "\n !!!STD_ERR:  " + erc.ToString(); 
+                }
+            });
         }
 
         void outputCollection_DataAdded(object sender, DataAddedEventArgs e)
@@ -112,7 +131,14 @@ namespace wpf_youtube_dl
                 Collection<PSObject> curr = outputCollection.ReadAll();
                 foreach(PSObject r in curr)
                 {
+
                     tb_Out.Text += "\n" + r.ToString();
+                    if (r.ToString().Contains("av01"))
+                    {
+                        cb_AV1.IsChecked = true;
+                        tb_Out.Text += "\n" + "↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑~AV1~";
+                    }
+
                     tb_Out.ScrollToEnd();
                 }
             }
@@ -124,6 +150,11 @@ namespace wpf_youtube_dl
             ps.Stop();
             ps = null;
             iasresult = null;
+        }
+
+        private void btn_OpenDest_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe" , @tb_Path.Text);
         }
     }
 }
